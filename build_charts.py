6c3101460aeb974a8d343779250f3ac1e982f953
Real Limgoes <b.real.limoges@gmail.com>
2015-05-06 08:51:15 -0400

    Add build_charts.py
    This is the main function for this program

---
+++ build_charts.py
@@ -0,0 +1,18 @@
+# -*- coding utf-8 -*-
+
+"""
+By: Real Limoges
+Last Updated: 5/6/15
+
+This file calls build_pie_charts and build_bar_charts to make charts for the
+data description paper for the PCRI database.
+
+Underlying data is not available to be pushed
+
+"""
+
+def main():
+
+	print "Hello World"
+
+main()
